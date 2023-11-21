27ff70406a2628896ab47318ff890227f6dee30c
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-11-21 15:57:38 +0100

    Update GoodName.py

--- GoodName.py
+++ GoodName.py
@@ -4,20 +4,14 @@ import sys
 folder_path = "TestFiles"
 file_list = os.listdir(folder_path)
 
-bad_files = []
-
 for file_name in file_list:
     file_path = os.path.join(folder_path, file_name)
 
     if os.path.isfile(file_path):
-        if not file_name.startswith('BusinessRule_ba_'):
-            print(f"Bad file: {file_path}")
-            bad_files.append(file_path)
-            
-if bad_files:
-    print("Filenames without 'BusinessRule_ba_':")
-    for bad_file in bad_files:
-        print(bad_file)
-    sys.exit(1) 
-else:
-    print("All files have the expected prefix. Processing complete.")
+
+        if file_name.startswith('BusinessRule_ba_'):
+            print(f"Good filename: {file_path}")
+        else:
+            print(f"Bad filename: {file_path}")
+            print("The filename should start with 'BusinessRule_ba_'")
+            sys.exit(1)
