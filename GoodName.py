3c3e84508e9d7079da8d0edf9961048442b41e7b
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-11-21 13:07:34 +0100

    Update GoodName.py

--- GoodName.py
+++ GoodName.py
@@ -7,9 +7,10 @@ for file_name in file_list:
     file_path = os.path.join(folder_path, file_name)
 
     if os.path.isfile(file_path):
-        # Check if the file starts with 'BusinessRule_ba_'
+
         if file_name.startswith('BusinessRule_ba_'):
             print(f"Processing file: {file_path}")
             print("Hello")
         else:
             print(f"Bad file: {file_path}")
+            print("Bad")
