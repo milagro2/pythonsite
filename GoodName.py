42cc5b612e656e922627bfde841dbb6eb580f8ce
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-11-21 11:06:29 +0100

    Update GoodName.py

--- GoodName.py
+++ GoodName.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import os
+
+folder_path = "TestFiles"
+file_list = os.listdir(folder_path)
+
+for file_name in file_list:
+    file_path = os.path.join(folder_path, file_name)
+
+    if os.path.isfile(file_path):
+
+      
+        print(f"Processing file: {file_path}")
+        
+
+        print("Hello")
