e97642a47c5c86d43aacf79fd780de80a7da010a
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-11-23 16:25:49 +0100

    Update VariableChecker.py

--- projects/project2/VariableChecker.py
+++ projects/project2/VariableChecker.py
@@ -1,57 +1,27 @@
 import json
 
-class JSONBlock:
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+def read_json_objects_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
 
-    @classmethod
-    def from_string(cls, json_str):
-        try:
-            json_obj = json.loads(json_str)
-            name = cls.get_block_name(json_obj)
-            return cls(name, json_obj)
-        except json.JSONDecodeError as e:
-            print(f"Error decoding JSON: {e}")
-            return None
+        # Parse JSON-formatted content into a list of dictionaries
+        json_objects = json.loads(content)
+        return json_objects
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+        return None
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
-    @staticmethod
-    def get_block_name(json_obj):
-        if "businessRule" in json_obj:
-            return "business_rule"
-        elif "exportMetadata" in json_obj:
-            return "export_metadata"
-        elif "businessRuleDefinition" in json_obj:
-            return "business_rule_definition"
-        else:
-            return "unknown"
+# Example usage:
+file_path = 'projects/project2/TestFiles/'
+result = read_json_objects_from_file(file_path)
 
-def get_json_blocks(filepath):
-    json_blocks = []
-
-    with open(filepath, 'r') as file:
-        for line in file:
-            json_start = line.find('{')
-            json_end = line.find('}')
-
-            while json_start != -1 and json_end != -1:
-                json_str = line[json_start:json_end + 1]
-
-                json_block = JSONBlock.from_string(json_str)
-
-                if json_block:
-                    json_blocks.append(json_block)
-
-                line = line[json_end + 1:]
-                json_start = line.find('{')
-                json_end = line.find('}')
-
-    return json_blocks
-
-filepath = 'projects/project2/TestFiles/BusinessRule_ba_InitiateInUpdateWFTIMS.js'
-result = get_json_blocks(filepath)
-
-for json_block in result:
-    print(f"Name: {json_block.name}")
-    print(f"Value: {json.dumps(json_block.value, indent=2)}")
-    print()
+if result is not None:
+    for obj in result:
+        print(obj)
