af9b914bc9fe8e708ab994f516c48f2640a0c576
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-11-23 14:23:38 +0100

    Update VariableChecker.py

--- projects/project2/VariableChecker.py
+++ projects/project2/VariableChecker.py
@@ -7,21 +7,27 @@ def read_json_from_folder(folder_path):
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
 
+        # Check if it's a file
         if os.path.isfile(file_path):
             with open(file_path, 'r') as file:
                 file_content = file.read()
 
-                json_start = file_content.find('{')
-                json_end = file_content.rfind('}') + 1
+                while True:
+                    json_start = file_content.find('{')
+                    json_end = file_content.find('}')
 
-                if json_start != -1 and json_end != -1:
-                    json_str = file_content[json_start:json_end]
+                    if json_start != -1 and json_end != -1:
+                        json_str = file_content[json_start:json_end + 1]
 
-                    try:
-                        json_object = json.loads(json_str)
-                        json_objects.append(json_object)
-                    except json.JSONDecodeError as e:
-                        print(f"Error decoding JSON in {filename}: {e}")
+                        try:
+                            json_object = json.loads(json_str)
+                            json_objects.append(json_object)
+                        except json.JSONDecodeError as e:
+                            print(f"Error decoding JSON in {filename}: {e}")
+
+                        file_content = file_content[json_end + 1:]
+                    else:
+                        break
 
     return json_objects
 
