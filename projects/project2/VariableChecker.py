33d53c12d4a248afc153bca742b656460a99d2b4
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-11-23 20:26:25 +0100

    Update and rename VariTest.py to VariableChecker.py

--- projects/project2/VariableChecker.py
+++ projects/project2/VariableChecker.py
@@ -5,7 +5,6 @@ def extract_json_objects(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
 
-    # Define patterns for extracting JSON objects within comments
     patterns = [
         r'/\*===== export metadata =====\s*({.*?})\s*\*/',
         r'/\*===== business rule definition =====\s*({.*?})\s*\*/',
@@ -14,7 +13,6 @@ def extract_json_objects(file_path):
 
     extracted_objects = {}
 
-    # Extract JSON objects using regular expressions
     for pattern in patterns:
         match = re.search(pattern, content, re.DOTALL)
         if match:
@@ -24,10 +22,8 @@ def extract_json_objects(file_path):
 
     return extracted_objects
 
-# Replace 'file_path' with the actual path to your file
 file_path = 'projects/project2/TestFiles/BusinessRule_ba_InitiateInUpdateWFTIMS.js'
 result = extract_json_objects(file_path)
 
-# Print the extracted JSON objects
 for label, json_object in result.items():
     print(f"\n{label}:\n{json.dumps(json_object, indent=2)}")
