145ae0556766341668ee36d3e7c2e7432d15751a
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-06 14:50:53 +0100

    Update WordFinder.py

--- projects/project4/WordFinder.py
+++ projects/project4/WordFinder.py
@@ -4,12 +4,23 @@ def extract_alias_value_pairs(lines):
     alias_value_pairs = []
     alias = None
 
+    contract_types = {
+        'ManagerBindContract': {'alias': 'manager', 'value': 'null'},
+        'CurrentObjectBindContract': {'alias': 'node', 'value': 'null'},
+        'AttributeBindContract': {'alias': '', 'value': ''}
+    }
+
+    current_contract = None
+
     for line in lines:
-        if 'alias' in line:
+        if 'contract' in line:
+            current_contract = line.strip(' ,"').split(':')[1].strip()
+
+        if 'alias' in line and current_contract in contract_types:
             alias = line.strip(' ,"').split(':')[1].strip()
-        elif 'value' in line and alias:
+        elif 'value' in line and alias is not None and current_contract in contract_types:
             value = line.strip(' ,"').split(':')[1].strip()
-            alias_value_pairs.append((alias, value))
+            alias_value_pairs.append((current_contract, alias, value))
             alias = None
 
     return alias_value_pairs
@@ -29,12 +40,12 @@ for file_name in file_list:
             if alias_value_pairs:
                 print(f"\nAlias-Value pairs in {file_name}:")
 
-                for alias, value in alias_value_pairs:
-                    print(f'alias: {alias}, value: {value}', end=' ')
+                for contract, alias, value in alias_value_pairs:
+                    print(f'Contract: {contract}, alias: {alias}, value: {value}', end=' ')
 
                     if alias == value:
                         print("alias is the same as value")
                     else:
                         print("----------------alias is not the same as value----------------")
 
-print("\nCheck complete.") 
+print("\nCheck complete.")
