437120cb9381486248e8aa36c5628764514101cb
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-06 14:39:57 +0100

    Update WordFinder.py

--- projects/project4/WordFinder.py
+++ projects/project4/WordFinder.py
@@ -1,5 +1,4 @@
-import os
-import json
+ import os
 
 def extract_alias_value_pairs(lines):
     alias_value_pairs = []
@@ -14,42 +13,6 @@ def extract_alias_value_pairs(lines):
             alias = None
 
     return alias_value_pairs
-
-def check_bind_contracts(file_path, extracted_objects):
-    issues = []
-
-    for label, json_object in extracted_objects.items():
-        if label == 'business rule plugin definition' and 'binds' in json_object:
-            for bind in json_object['binds']:
-                contract = bind.get('contract', '')
-                alias = bind.get('alias', '')
-                value = bind.get('value', '')
-
-                if contract == 'AttributeBindContract':
-                    if alias != value:
-                        issues.append({
-                            'file_name': file_path,
-                            'bind_alias': alias,
-                            'bind_issue': 'Alias is not the same as value'
-                        })
-
-                elif contract == 'CurrentObjectBindContract':
-                    if alias != 'node':
-                        issues.append({
-                            'file_name': file_path,
-                            'bind_alias': alias,
-                            'bind_issue': 'Alias is not "node" for CurrentObjectBindContract'
-                        })
-
-                elif contract == 'ManagerBindContract':
-                    if alias != 'manager':
-                        issues.append({
-                            'file_name': file_path,
-                            'bind_alias': alias,
-                            'bind_issue': 'Alias is not "manager" for ManagerBindContract'
-                        })
-
-    return issues
 
 folder_path = "projects/project4/TestFiles"
 file_list = os.listdir(folder_path)
@@ -74,12 +37,4 @@ for file_name in file_list:
                     else:
                         print("----------------alias is not the same as value----------------")
 
-                extracted_objects = extract_json_objects(file_path)
-                bind_issues = check_bind_contracts(file_path, extracted_objects)
-
-                if bind_issues:
-                    print("\nBind Issues:")
-                    for issue in bind_issues:
-                        print(f"File: {issue['file_name']}, Bind Alias: {issue['bind_alias']}, Issue: {issue['bind_issue']}")
-
-print("\nCheck complete.")
+print("\nCheck complete.") 
