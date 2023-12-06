9faff146f5a7e118838b5e6d92c93025bd7dade6
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-06 13:52:19 +0100

    Update WordFinder.py

--- projects/project4/WordFinder.py
+++ projects/project4/WordFinder.py
@@ -12,21 +12,19 @@ for file_name in file_list:
             lines = file.readlines()
             alias_lines = [line.strip() for line in lines if 'alias' in line]
             value_lines = [line.strip() for line in lines if 'value' in line]
-            
-            print(f"\nChecking aliases and values in {file_name}:")
-            
-            for i in range(len(alias_lines)):
-                alias_line = alias_lines[i]
-                value_line = value_lines[i] if i < len(value_lines) else None
-                
-                print(f'"{alias_line}", "{value_line}"')
-                
-                alias_value = alias_line.split('"value"')[0].strip()
-                value_value = value_line.split('"value"')[1].strip() if value_line else None
-                
-                if alias_value == value_value:
-                    print('Alias is the same as value')
-                else:
-                    print('Alias is not the same as value')
+            if alias_lines:
+                print(f"\nLines with 'alias' followed by 'value' in {file_name}:")
+                for alias_line, value_line in zip(alias_lines, value_lines):
+                    alias_value_pair = f"{alias_line}, {value_line}"
+
+                    alias = alias_line.split('"')[3]
+                    value = value_line.split('"')[3]
+
+                    print(alias_value_pair)
+
+                    if alias == value:
+                        print("Alias is the same as value")
+                    else:
+                        print("Alias is not the same as value")
 
 print("\nCheck complete.")
