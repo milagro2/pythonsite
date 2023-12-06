b439beb5233d41ffff961ab643365187e1c4babc
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-06 13:49:42 +0100

    Update WordFinder.py

--- projects/project4/WordFinder.py
+++ projects/project4/WordFinder.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 folder_path = "projects/project4/TestFiles"
 file_list = os.listdir(folder_path)
@@ -9,23 +10,23 @@ for file_name in file_list:
     if os.path.isfile(file_path):
         with open(file_path, 'r') as file:
             lines = file.readlines()
-
-            alias_and_value_lines = []
-            current_object = ""
-            value_found = False
-
-            for line in lines:
-                if 'alias' in line:
-                    current_object = line.strip()
-                    value_found = False
-                elif 'value' in line and not value_found:
-                    current_object += ' ' + line.strip()
-                    alias_and_value_lines.append(current_object)
-                    value_found = True
-
-            if alias_and_value_lines:
-                print(f"\nLines with 'alias' followed by 'value' in {file_name}:")
-                for line in alias_and_value_lines:
-                    print(line)
+            alias_lines = [line.strip() for line in lines if 'alias' in line]
+            value_lines = [line.strip() for line in lines if 'value' in line]
+            
+            print(f"\nChecking aliases and values in {file_name}:")
+            
+            for i in range(len(alias_lines)):
+                alias_line = alias_lines[i]
+                value_line = value_lines[i] if i < len(value_lines) else None
+                
+                print(f'"{alias_line}", "{value_line}"')
+                
+                alias_value = alias_line.split('"value"')[0].strip()
+                value_value = value_line.split('"value"')[1].strip() if value_line else None
+                
+                if alias_value == value_value:
+                    print('Alias is the same as value')
+                else:
+                    print('Alias is not the same as value')
 
 print("\nCheck complete.")
