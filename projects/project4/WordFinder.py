bb4242c2b0709666068150fbfcd38c45900e34d9
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-06 13:14:11 +0100

    Update WordFinder.py

--- projects/project4/WordFinder.py
+++ projects/project4/WordFinder.py
@@ -10,9 +10,9 @@ for file_name in file_list:
     if os.path.isfile(file_path):
         with open(file_path, 'r') as file:
             lines = file.readlines()
-            alias_lines = [line.strip() for line in lines if 'alias' in line]
+            alias_lines = [line.strip() for line in lines if 'value' in line]
             if alias_lines:
-                print(f"\nLines with 'alias' in {file_name}:")
+                print(f"\nLines with 'value' in {file_name}:")
                 for alias_line in alias_lines:
                     print(alias_line)
 
