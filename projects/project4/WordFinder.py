1e92af014a056f07d5a120d101cccf08db3907b5
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-07 11:37:04 +0100

    Update WordFinder.py

--- projects/project4/WordFinder.py
+++ projects/project4/WordFinder.py
@@ -1,9 +1,7 @@
 import os
-
 def extract_alias_value_pairs(lines, contract):
     alias_value_pairs = []
     alias = None
-
     for line in lines:
         if 'alias' in line:
             alias = line.strip(' ,"').split(':')[1].strip()
@@ -11,15 +9,11 @@ def extract_alias_value_pairs(lines, contract):
             value = line.strip(' ,"').split(':')[1].strip()
             alias_value_pairs.append((alias, value))
             alias = None
-
     return alias_value_pairs
-
 folder_path = "projects/project4/TestFiles"
 file_list = os.listdir(folder_path)
-
 for file_name in file_list:
     file_path = os.path.join(folder_path, file_name)
-
     if os.path.isfile(file_path):
         with open(file_path, 'r') as file:
             lines = file.readlines()
@@ -31,26 +25,13 @@ for file_name in file_list:
 
             if alias_value_pairs:
                 print(f"\nAlias-Value pairs in {file_name}:")
-
                 for alias, value in alias_value_pairs:
                     print(f'alias: {alias}, value: {value}', end=' ')
-
-                    if contract == 'ManagerBindContract':
-                        if alias == 'manager':
-                            print("alias is correct")
-                        else:
-                            print("----------------alias should be 'manager'----------------")
                     if alias == value:
                         print("alias and value are correct")
                     else:
-                        if alias == value:
-                            print("alias and value are correct")
-                        else:
-                            print("----------------alias or value is not correct----------------")
-
                         print("----------------alias or value is not correct----------------")
                 if contract == 'CurrentObjectBindContract':
                     print("Special case: node and null")
                 elif contract == 'ManagerBindContract':
                     print("Maaaa case: manager and null")
-print("\nCheck complete.")
