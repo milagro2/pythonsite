17338d3eaff7090ad37630b622d42648602df17c
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-07 11:32:44 +0100

    Update WordFinder.py

--- projects/project4/WordFinder.py
+++ projects/project4/WordFinder.py
@@ -1,4 +1,5 @@
 import os
+
 def extract_alias_value_pairs(lines, contract):
     alias_value_pairs = []
     alias = None
@@ -10,17 +11,19 @@ def extract_alias_value_pairs(lines, contract):
             alias_value_pairs.append((alias, value))
             alias = None
     return alias_value_pairs
+
 folder_path = "projects/project4/TestFiles"
 file_list = os.listdir(folder_path)
+
 for file_name in file_list:
     file_path = os.path.join(folder_path, file_name)
+
     if os.path.isfile(file_path):
         with open(file_path, 'r') as file:
             lines = file.readlines()
 
-            contract = 'CurrentObjectBindContract'
             contract = 'ManagerBindContract'
-
+            
             alias_value_pairs = extract_alias_value_pairs(lines, contract)
 
             if alias_value_pairs:
@@ -28,11 +31,13 @@ for file_name in file_list:
                 for alias, value in alias_value_pairs:
                     print(f'alias: {alias}, value: {value}', end=' ')
                     if alias == value:
-                        print("alias and value are correct")
+                        print("alias and value are correct", end=' ')
+
+                        if contract == 'CurrentObjectBindContract' and alias == 'node' and value == 'null':
+                            print("Special case: node and null")
+                        elif contract == 'ManagerBindContract' and alias == 'manager' and value == 'null':
+                            print("Special case: manager and null")
                     else:
                         print("----------------alias or value is not correct----------------")
-                if contract == 'CurrentObjectBindContract':
-                    print("Special case: node and null")
-                elif contract == 'ManagerBindContract':
-                    print("Maaaa case: manager and null")
+
 print("\nCheck complete.")
