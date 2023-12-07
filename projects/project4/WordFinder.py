9ae8feed5424842dd0a294a94bb822c084fe341e
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-07 13:26:32 +0100

    Update WordFinder.py

--- projects/project4/WordFinder.py
+++ projects/project4/WordFinder.py
@@ -1,18 +1,25 @@
 import os
 
-def extract_alias_value_pairs(lines, contract):
-    alias_value_pairs = []
+def extract_alias_value_contract_triplets(lines):
+    alias_value_contract_triplets = []
     alias = None
+    value = None
+    contract = None
 
     for line in lines:
         if 'alias' in line:
             alias = line.strip(' ,"').split(':')[1].strip()
         elif 'value' in line and alias:
             value = line.strip(' ,"').split(':')[1].strip()
-            alias_value_pairs.append((alias, value))
-            alias = None
+        elif 'contract' in line and alias and value:
+            contract = line.strip(' ,"').split(':')[1].strip()
 
-    return alias_value_pairs
+            alias_value_contract_triplets.append((alias, value, contract))
+            alias = None
+            value = None
+            contract = None
+
+    return alias_value_contract_triplets
 
 folder_path = "projects/project4/TestFiles"
 file_list = os.listdir(folder_path)
@@ -24,24 +31,22 @@ for file_name in file_list:
         with open(file_path, 'r') as file:
             lines = file.readlines()
 
-            contract = 'CurrentObjectBindContract'
+            alias_value_contract_triplets = extract_alias_value_contract_triplets(lines)
 
-            alias_value_pairs = extract_alias_value_pairs(lines, contract)
+            if alias_value_contract_triplets:
+                print(f"\nAlias-Value-Contract triplets in {file_name}:")
 
-            if alias_value_pairs:
-                print(f"\nAlias-Value pairs in {file_name}:")
-
-                for alias, value in alias_value_pairs:
-                    print(f'alias: {alias}, value: {value}', end=' ')
+                for alias, value, contract in alias_value_contract_triplets:
+                    print(f'alias: {alias}, value: {value}, contract: {contract}', end=' ')
 
                     if alias == value:
                         print("alias and value are correct")
                     else:
                         print("----------------alias or value is not correct----------------")
 
-                if contract == 'CurrentObjectBindContract':
-                    print("Special case: node and null")
-                elif contract == 'ManagerBindContract':
-                    print("Special case: manager and null")
+                    if contract == 'CurrentObjectBindContract':
+                        print("Special case: node and null")
+                    elif contract == 'ManagerBindContract':
+                        print("Special case: manager and null")
 
 print("\nCheck complete.")
