ffe9c6e314d3fc3989665f8f920933c1eef2bb81
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-07 10:03:33 +0100

    This is the correct one to go back to

--- projects/project4/WordFinder.py
+++ projects/project4/WordFinder.py
@@ -1,7 +1,9 @@
 import os
+
 def extract_alias_value_pairs(lines):
     alias_value_pairs = []
     alias = None
+
     for line in lines:
         if 'alias' in line:
             alias = line.strip(' ,"').split(':')[1].strip()
@@ -9,25 +11,30 @@ def extract_alias_value_pairs(lines):
             value = line.strip(' ,"').split(':')[1].strip()
             alias_value_pairs.append((alias, value))
             alias = None
+
     return alias_value_pairs
+
 folder_path = "projects/project4/TestFiles"
 file_list = os.listdir(folder_path)
+
 for file_name in file_list:
     file_path = os.path.join(folder_path, file_name)
+
     if os.path.isfile(file_path):
         with open(file_path, 'r') as file:
             lines = file.readlines()
+
             alias_value_pairs = extract_alias_value_pairs(lines)
+
             if alias_value_pairs:
                 print(f"\nAlias-Value pairs in {file_name}:")
+
                 for alias, value in alias_value_pairs:
-                    print(f'{alias}, {value}', end=' ')
+                    print(f'alias: {alias}, value: {value}', end=' ')
 
                     if alias == value:
-                        print("alias is same as value")
-                        print("alias is the same as value")
+                        print("alias and value are correct")
                     else:
-                        print("alias is the same as value")
-                        print("------------------------------------alias is not the same as value------------------------------------")
+                        print("----------------alias or value is not correct----------------")
 
-print("\nCheck complete.")
+print("\nCheck complete.")  
