fef63f08c46ef7bb075adb7e82b7536e4404564e
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-07 13:21:52 +0100

    Update WordFinder.py

--- projects/project4/WordFinder.py
+++ projects/project4/WordFinder.py
@@ -3,6 +3,7 @@ import os
 def extract_alias_value_pairs(lines, contract):
     alias_value_pairs = []
     alias = None
+
     for line in lines:
         if 'alias' in line:
             alias = line.strip(' ,"').split(':')[1].strip()
@@ -10,6 +11,7 @@ def extract_alias_value_pairs(lines, contract):
             value = line.strip(' ,"').split(':')[1].strip()
             alias_value_pairs.append((alias, value))
             alias = None
+
     return alias_value_pairs
 
 folder_path = "projects/project4/TestFiles"
@@ -22,34 +24,24 @@ for file_name in file_list:
         with open(file_path, 'r') as file:
             lines = file.readlines()
 
-            # Set the contracts based on your requirement
-            current_object_contract = 'CurrentObjectBindContract'
-            manager_contract = 'ManagerBindContract'
+            contract = 'CurrentObjectBindContract'
 
-            alias_value_pairs_current = extract_alias_value_pairs(lines, current_object_contract)
-            alias_value_pairs_manager = extract_alias_value_pairs(lines, manager_contract)
+            alias_value_pairs = extract_alias_value_pairs(lines, contract)
 
-            if alias_value_pairs_current or alias_value_pairs_manager:
+            if alias_value_pairs:
                 print(f"\nAlias-Value pairs in {file_name}:")
 
-                for alias, value in alias_value_pairs_current:
+                for alias, value in alias_value_pairs:
                     print(f'alias: {alias}, value: {value}', end=' ')
+
                     if alias == value:
                         print("alias and value are correct")
                     else:
                         print("----------------alias or value is not correct----------------")
 
-                for alias, value in alias_value_pairs_manager:
-                    print(f'alias: {alias}, value: {value}', end=' ')
-                    if alias == value:
-                        print("Maaaa case: alias and value are correct")
-                    else:
-                        print("Maaaa case: ----------------alias or value is not correct----------------")
-
-                # Check for specific contracts and print corresponding messages
-                if current_object_contract in file_name:
+                if contract == 'CurrentObjectBindContract':
                     print("Special case: node and null")
-                elif manager_contract in file_name:
+                elif contract == 'ManagerBindContract':
                     print("Special case: manager and null")
 
 print("\nCheck complete.")
