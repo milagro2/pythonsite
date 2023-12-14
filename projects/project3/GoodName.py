de0f549638cd8ec278e8c7e2fe09106995fead16
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-14 09:39:18 +0100

    Update GoodName.py

--- projects/project3/GoodName.py
+++ projects/project3/GoodName.py
@@ -26,7 +26,6 @@ if bad_files:
     for bad_file in bad_files:
         print(bad_file)
     print("\nError: We found files that violate the naming convention")
-    # sys.exit(1)
 
 else:
     print("\nAll filenames are correct.")
